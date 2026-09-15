dc27022d05881d5b8349b8322edb78e62306d914
frostigerfrosty <laurin.gloor@stud.edubs.ch>
2026-09-15 18:14:44 +0200

    changes for the static values on the page

--- js/script.js
+++ js/script.js
@@ -6,6 +6,49 @@ const dashboard = {
     winRate: 61.2
 };
 
+// Bets
+const bets = [
+    {
+        team1: "Arsenal",
+        team2: "Chelsea",
+        type: "Match Winner",
+        odds: 1.72,
+        stake: 20,
+        result: "win",
+        profit: 14.40
+    },
+
+    {
+        team1: "Barcelona",
+        team2: "Sevilla",
+        type: "Over 2.5",
+        odds: 1.84,
+        stake: 15,
+        result: "loss",
+        profit: -15
+    },
+
+    {
+        team1: "Liverpool",
+        team2: "Brighton",
+        type: "Match Winner",
+        odds: 1.55,
+        stake: 25,
+        result: "win",
+        profit: 13.75
+    },
+
+    {
+        team1: "Real Madrid",
+        team2: "Valencia",
+        type: "BTTS",
+        odds: 1.80,
+        stake: 10,
+        result: "pending",
+        profit: 0
+    }
+];
+
 
 //writing the values into HTML
 document.getElementById("bankroll").textContent =
